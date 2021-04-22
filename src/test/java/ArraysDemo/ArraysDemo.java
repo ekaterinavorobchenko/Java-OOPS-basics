9e8b836f025c1280439c5de63f1bbd2abef5a774
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-22 13:42:25 +0300

    Single dimensional and Multidimensional arrays usage

--- src/test/java/ArraysDemo/ArraysDemo.java
+++ src/test/java/ArraysDemo/ArraysDemo.java
@@ -11,9 +11,11 @@ public class ArraysDemo {
         a[2] = 1;
         a[3] = 9;
         a[4] = 12;
-        for (int i=0;i<a.length;i++)
+
+        int b[] = {1,4,3,5,7,8};
+        for (int i=0;i<b.length;i++)
         {
-            System.out.println(a[i]);
+            System.out.println(b[i]);
         }
     }
 }
