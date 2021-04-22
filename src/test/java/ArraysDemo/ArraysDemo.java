559ebb672edb4815bec86cea193a2d6bde300130
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-22 13:07:54 +0300

    Arrays

---
+++ src/test/java/ArraysDemo/ArraysDemo.java
@@ -0,0 +1,19 @@
+package ArraysDemo;
+
+public class ArraysDemo {
+    public static void main (String[] args)
+    {
+        /*int a;
+        a =4;*/
+        int a[] = new int[5];
+        a[0] = 2;
+        a[1] = 6;
+        a[2] = 1;
+        a[3] = 9;
+        a[4] = 12;
+        for (int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
