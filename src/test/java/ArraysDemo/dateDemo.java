bfb5d4cd146ad82380f6a69225048f54ac853d9d
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-22 15:19:50 +0300

    Date class concepts

---
+++ src/test/java/ArraysDemo/dateDemo.java
@@ -0,0 +1,17 @@
+package ArraysDemo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class dateDemo {
+    public static void main(String[] args){
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
+        SimpleDateFormat sd = new SimpleDateFormat("M/d/yyyy hh:mm:ss");
+        System.out.println(sdf.format(d));
+        System.out.println(sd.format(d));
+        System.out.println(d.toString());
+
+
+    }
+}
